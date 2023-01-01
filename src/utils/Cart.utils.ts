ac5a979f07075314c00880b964fc5b834b9189ea
cmario92 <c_mario7@hotmail.com>
2023-01-01 18:32:25 +0200

    cart utils

--- src/utils/Cart.utils.ts
+++ src/utils/Cart.utils.ts
@@ -7,7 +7,7 @@ export const createCartActionsAndGetCart = async (
 	cart: ICartResponse,
 	product: IProductInterface,
 	quantity: number,
-	updateCart: (cart: ICartResponse) => void
+	updateCart: (cart: ICartResponse) => void,
 ) => {
 	// create a new CartItem
 	const cartItem: CartItem = {
@@ -17,12 +17,11 @@ export const createCartActionsAndGetCart = async (
 		product: product,
 	};
 	// create cart item and refresh GET Cart
-	await createCartItem(cartItem).then((res) => {
-		if (res?.statusText === 'OK') {
+	await createCartItem(cartItem).then((res: any) => {
+		if (res['statusText'] === 'OK') {
 			// call the PUT to update the card with the new CartItem
-			getCart(`${cart?.id}`).then((responseData) => {
-				const resData = responseData?.data.data;
-				updateCart(resData);
+			getCart(`${cart?.id}`).then((responseData: any) => {
+				updateCart(responseData?.data.data);
 			});
 		}
 	});
@@ -32,21 +31,18 @@ export const updateCartActionAndGetCart = async (
 	tmpQuantity: number,
 	cartItem: CartItemResponse,
 	cart: ICartResponse,
-	updateCart: (cart: ICartResponse) => void
+	updateCart: (cart: ICartResponse) => void,
 ) => {
 	const tmpCartItem: CartItemPayload = {
 		quantity: tmpQuantity,
-		price: calculatePriceWithQuantity(
-			cartItem.attributes.product.data.attributes.price,
-			tmpQuantity
-		),
+		price: calculatePriceWithQuantity(cartItem.attributes.product.data.attributes.price, tmpQuantity),
 		product: cartItem.attributes.product,
 		cart: cartItem.attributes.cart,
 		product_discount: cartItem.attributes.product_discount,
 	};
-	await updateCartItem(`${cartItem.id}`, tmpCartItem).then((res) => {
+	await updateCartItem(`${cartItem.id}`, tmpCartItem).then((res: any) => {
 		if (res?.statusText === 'OK') {
-			getCart(`${cart?.id}`).then((responseData) => {
+			getCart(`${cart?.id}`).then((responseData: any) => {
 				const resData = responseData?.data.data;
 				updateCart(resData);
 			});
