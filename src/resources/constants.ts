df1ba17faced022261852773791539f531263f9c
cmario92 <c_mario7@hotmail.com>
2023-01-01 16:18:14 +0200

    more configurations for linting

--- src/resources/constants.ts
+++ src/resources/constants.ts
@@ -1,14 +1,14 @@
 export const PRODUCTS = [
-  {
-    id: 1,
-    title: 'A simple product',
-    subTitle: 'Try it you will not miss it',
-    price: 12,
-  },
-  {
-    id: 2,
-    title: 'A SENIOR product',
-    subTitle: 'Try it you will not miss it',
-    price: 14,
-  },
-]
+	{
+		id: 1,
+		title: 'A simple product',
+		subTitle: 'Try it you will not miss it',
+		price: 12,
+	},
+	{
+		id: 2,
+		title: 'A SENIOR product',
+		subTitle: 'Try it you will not miss it',
+		price: 14,
+	},
+];
