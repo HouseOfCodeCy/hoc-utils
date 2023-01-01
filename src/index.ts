622f8cbaf4fc29b963e9d33d46aff65c004a3cd8
Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
2023-01-01 15:40:07 +0000

    CI: Bumps Version to 1.2.1-0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-import { calculatePrice, calculatePriceWithQuantity, calculateProductCategories, calculateTotalDiscount, calculateTotalPrice, quantityHandle } from './utils/Product.utils';
+import {
+	calculatePrice,
+	calculatePriceWithQuantity,
+	calculateProductCategories,
+	calculateTotalDiscount,
+	calculateTotalPrice,
+	quantityHandle,
+} from './utils/Product.utils';
 
-export { calculateTotalDiscount, calculateTotalPrice, calculatePrice, calculatePriceWithQuantity, calculateProductCategories, quantityHandle };
-
+export {
+	calculateTotalDiscount,
+	calculateTotalPrice,
+	calculatePrice,
+	calculatePriceWithQuantity,
+	calculateProductCategories,
+	quantityHandle,
+};
