4ca6ecf7ef849b71358a9f4e555b538f0e09827e
cmario92 <c_mario7@hotmail.com>
2023-01-02 18:30:58 +0200

    comment out interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // Interfaces
-export * from './interfaces/cart';
-export * from './interfaces/product';
+// export * from './interfaces/cart';
+// export * from './interfaces/product';
 // Services
 export * from './services/index';
 // Utility Functions
