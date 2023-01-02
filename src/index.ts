14e78c99a7e58ecc2212abff315035848817b74f
cmario92 <c_mario7@hotmail.com>
2023-01-02 15:09:18 +0200

    exporting index from services

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 export * as CartInterfaces from './interfaces/cart';
 export * as ProductInterfaces from './interfaces/product';
 // Services
-export * as CartService from './services/Cart.service';
+export * as CartService from './services/index';
 // Utility Functions
 export * as cartUtils from './utils/Cart.utils';
 export * as productUtils from './utils/Product.utils';
---
+++ src/services/index.ts
@@ -0,0 +1,1 @@
+export { createCart, createCartItem, deleteCart, getCart, updateCart, updateCartItem } from './Cart.service';
