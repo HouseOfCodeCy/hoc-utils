bca4911812cd5e240bd16e22574f6f659b574b24
cmario92 <c_mario7@hotmail.com>
2023-01-07 21:56:50 +0200

    tranformUserFlatToUser should always return IUserFlat

--- src/utils/Account.utils.ts
+++ src/utils/Account.utils.ts
@@ -1,16 +1,13 @@
 import { IUser, IUserFlat } from '../interfaces/account';
 
-export const tranformUserFlatToUser = (userFlat: IUserFlat): IUser | null => {
-	if (userFlat) {
-		const { id, ...userRest } = userFlat;
-		const newIUser: IUser = {
-			id: id,
-			attributes: {
-				...userRest,
-			},
-		};
+export const tranformUserFlatToUser = (userFlat: IUserFlat): IUser => {
+	const { id, ...userRest } = userFlat;
+	const newIUser: IUser = {
+		id: id,
+		attributes: {
+			...userRest,
+		},
+	};
 
-		return newIUser;
-	}
-	return null;
+	return newIUser;
 };
