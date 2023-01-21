13114f0af28e4ad5e8cc145d4ebacc195a4d23ab
cmario92 <c_mario7@hotmail.com>
2023-01-21 22:10:54 +0200

    fix accepting string instead of Date

--- src/utils/Common.utils.ts
+++ src/utils/Common.utils.ts
@@ -1,9 +1,9 @@
 import { format } from 'date-fns';
 
-export const formatDate = (date: Date, formatter: string) => {
+export const formatDate = (dateString: string, formatter: string) => {
 	let formattedDate;
 	// format(date, "dd/MM/yyyy'T'HH:mm:ss.SSSxxx");
-	format(date, formatter);
+	format(new Date(dateString), formatter);
 
 	return formattedDate;
 };
