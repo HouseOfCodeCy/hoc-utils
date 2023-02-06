19886f20531acf031f410dfec7dc6009a51e01cf
cmario92 <c_mario7@hotmail.com>
2023-02-07 00:11:24 +0200

    fix method in order utils

--- src/utils/Order.utils.ts
+++ src/utils/Order.utils.ts
@@ -3,10 +3,8 @@ import { IShippingMethod } from '../interfaces/order';
 export const getDefaultShippingMethod = (
 	shippingMethods: IShippingMethod[],
 ) => {
-	if (shippingMethods && shippingMethods.length > 0) {
-		const defaultShippingMethod = shippingMethods.find(
-			(method) => method.attributes.isDefault === true,
-		);
-		return defaultShippingMethod;
-	}
+	const defaultShippingMethod = shippingMethods?.find(
+		(method) => method.attributes.isDefault === true,
+	);
+	return defaultShippingMethod;
 };
