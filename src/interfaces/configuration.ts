eb6d15ff86b522385b1104885ae0f6e8c1a59d6f
cmario92 <c_mario7@hotmail.com>
2023-02-12 21:37:36 +0200

    adding update properties to configurationInterface for context

--- src/interfaces/configuration.ts
+++ src/interfaces/configuration.ts
@@ -1,4 +1,16 @@
 export interface ConfigurationInterface {
 	guestCheckout: boolean;
 	addToCartNeedsAccount: boolean;
+	highAvailabilityThreshold: number;
+	lowAvailabilityThreshold: number;
+	showReviews: boolean;
+	shopFullName: string;
+	shopShortName: string;
+	shopDescription: string;
+	shopShortDescription: string;
+	favicon: string;
+	// inventoryReservationDuration (minutes)
+	inventoryReservationDuration: number;
+	// whether to show stock on categories and product
+	showStockOnCategories: boolean;
 }
