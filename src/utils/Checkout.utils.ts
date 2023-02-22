a1e4a9dc84493219e53cf6962416ec41db26ef49
cmario92 <c_mario7@hotmail.com>
2023-02-22 21:39:43 +0200

    sending full object in checkout not just IDs

--- src/utils/Checkout.utils.ts
+++ src/utils/Checkout.utils.ts
@@ -26,12 +26,11 @@ export const checkoutCartAndUpdateProductInventory = async (
 	orderPayload: any,
 ) => {
 	const newOrderPayload = {
-		cart: orderPayload.cart.id,
-		address: orderPayload.address ? orderPayload.address.id : null,
-		order_payment_method: orderPayload.order_payment_method.id,
-		shipping_method_option: orderPayload.shipping_method_option.id,
-		order_status: orderPayload.order_status.id,
-		user: orderPayload.user.id,
+		cart: orderPayload.cart,
+		address: orderPayload.address ? orderPayload.address : null,
+		order_payment_method: orderPayload.order_payment_method,
+		shipping_method_option: orderPayload.shipping_method_option,
+		order_status: orderPayload.order_status,
 	};
 	const orderResponse: any = await createOrder(newOrderPayload);
 	const cartId = orderPayload.cart.id;
