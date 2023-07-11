c3ce11b9a71f7757ad6ac4c7031bf0728203989a
cmario92 <c_mario7@hotmail.com>
2023-07-11 11:32:42 +0300

    get sections by order and enhancing the API

--- src/services/Sections.service.ts
+++ src/services/Sections.service.ts
@@ -1,11 +1,23 @@
-import { PopulateType } from '../resources/enums';
+import qs from 'qs';
+import { PopulateType, SortType } from '../resources/enums';
 import { http } from './common/Http.service';
 
-export const getSections = async (populateType = PopulateType.DEEP) => {
+export const getSections = async (
+	populateType = PopulateType.DEEP,
+	sortType: SortType[] = [SortType.ORDER_ASC],
+) => {
 	try {
-		const response = await http.get<any>(`home-sections`, {
-			params: { populate: populateType },
-		});
+		const query = qs.stringify(
+			{
+				sort: sortType,
+				populate: populateType,
+			},
+			{
+				encodeValuesOnly: true, // prettify URL
+			},
+		);
+		const response = await http.get<any>(`home-sections?${query}`);
+
 		return response;
 	} catch (error) {
 		console.log('unexpected error: ', error);
